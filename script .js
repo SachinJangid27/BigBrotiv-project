ac647d4972c2ca3fa7d4f1bb69e195857a7d5e35
SachinJangid27 <sj542759@gmail.com>
2026-02-03 17:22:10 +0530

    update

--- script .js
+++ script .js
@@ -42,6 +42,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const navbar = document.querySelector(".navbar");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
   });
+
+
+
+
+
+
 </script>
-
-
